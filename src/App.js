70eabd959cfd1335d4d7cd9e8de6160ebb62061a
codephillip <codephillip@gmail.com>
2021-03-10 10:28:53 +0300

    added Task component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import {useState} from 'react'
 
 // class based component
 // class App extends React.Component {
@@ -14,16 +15,39 @@ import Tasks from './components/Tasks'
 // function based component
 // function App() {
 const App = () => {
-  return (
-    <div className="container">
-      <Header title="page1"/>
-      <h1>hello react app</h1>
-      <Tasks/>
-    </div>
-  );
+    const [tasks, setTasks] = useState(
+        [
+            {
+                id: 1,
+                text: "go shopping tmox",
+                day: "Mar 9th at 3:40pm",
+                reminder: true
+            },
+            {
+                id: 2,
+                text: "read book",
+                day: "Mar 10th at 1:20pm",
+                reminder: false
+            },
+            {
+                id: 3,
+                text: "write blog post",
+                day: "Mar 10th at 5:20am",
+                reminder: true
+            }
+        ]
+        // updating tasks
+        // setTasks([...tasks, {id: 4, text: "buy shoes", day: "Mar 20th at 3:09pm", reminder: true}])
+
+    )
+    return (
+        <div className="container">
+            <Header title="page1"/>
+            <h1>hello react app</h1>
+            <Tasks tasks={tasks}/>
+        </div>
+    );
 }
-
-
 
 
 export default App;
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,37 +1,15 @@
-import { useState } from 'react'
+import Task from './Task'
 
-const [tasks, setTasks] = useState(
-    [
-        {
-            id: 1,
-            text: "go shopping tmox",
-            day: "Mar 9th at 3:40pm",
-            reminder: true
-        },
-        {
-            id: 2,
-            text: "read book",
-            day: "Mar 10th at 1:20pm",
-            reminder: false
-        },
-        {
-            id: 3,
-            text: "write blog post",
-            day: "Mar 10th at 5:20am",
-            reminder: true
-        }
-    ]
-)
-
-const Tasks = () => {
+const Tasks = ({tasks}) => {
     return (
         <>
-        {
-            tasks.map((task) => (
-                // item has to be unique add key attribute
-                <h3 key={task.id}>{task.id} - {task.text}</h3>
-            ))
-        }
+            {
+                tasks.map((task) => (
+                    // item has to be unique add key attribute
+                    // <h3 key={task.id}>{task.id} - {task.text}</h3>
+                    <Task key={task.id} task={task}/>
+                ))
+            }
         </>
     )
 }
