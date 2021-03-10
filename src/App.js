63702245de978cb7540d2ee4854581312501c015
codephillip <codephillip@gmail.com>
2021-03-10 15:34:19 +0300

    added get and delete tasks from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import AddTask from "./components/AddTask";
 
 // class based component
@@ -17,31 +17,51 @@ import AddTask from "./components/AddTask";
 // function App() {
 const App = () => {
     const [showAddForm, setShowAddForm] = useState(false)
-    const [tasks, setTasks] = useState(
-        [
-            {
-                id: 1,
-                text: "go shopping tmox",
-                day: "Mar 9th at 3:40pm",
-                reminder: true
-            },
-            {
-                id: 2,
-                text: "read book",
-                day: "Mar 10th at 1:20pm",
-                reminder: false
-            },
-            {
-                id: 3,
-                text: "write blog post",
-                day: "Mar 10th at 5:20am",
-                reminder: true
-            }
-        ]
-        // updating tasks
-        // setTasks([...tasks, {id: 4, text: "buy shoes", day: "Mar 20th at 3:09pm", reminder: true}])
+    // const [tasks, setTasks] = useState(
+    //     [
+    //         {
+    //             id: 1,
+    //             text: "go shopping tmox",
+    //             day: "Mar 9th at 3:40pm",
+    //             reminder: true
+    //         },
+    //         {
+    //             id: 2,
+    //             text: "read book",
+    //             day: "Mar 10th at 1:20pm",
+    //             reminder: false
+    //         },
+    //         {
+    //             id: 3,
+    //             text: "write blog post",
+    //             day: "Mar 10th at 5:20am",
+    //             reminder: true
+    //         }
+    //     ]
+    //     // updating tasks
+    //     // setTasks([...tasks, {id: 4, text: "buy shoes", day: "Mar 20th at 3:09pm", reminder: true}])
+    //
+    // )
 
-    )
+    const [tasks, setTasks] = useState([])
+
+    useEffect(() => {
+        const getTasks = async () => {
+            // use await because it is a promise
+            const newTasks = await fetchTasks()
+            setTasks(newTasks)
+        }
+
+        getTasks()
+        //dependencies trigger reload of data when they change
+    },[])
+
+    const fetchTasks = async () => {
+        const res = await fetch('http://localhost:9000/data')
+        const data = await res.json()
+        console.log(data)
+        return data
+    }
 
     const addNewTask = (task) => {
         // console.log('add', task)
@@ -53,8 +73,12 @@ const App = () => {
         setTasks([...tasks, newTask])
     }
 
-    const deleteTask = (id) => {
+    const deleteTask = async (id) => {
         console.log('delete', id)
+        // use template string
+        await fetch(`http://localhost:9000/data/${id}`, {
+            method: "DELETE"
+        })
         setTasks(tasks.filter((task) => task.id !== id))
     }
 
